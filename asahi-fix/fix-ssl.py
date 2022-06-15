aec16970c9efb273daff22b9b5c94efdc8fc2fe0
op06072 <op06072@gmail.com>
2022-06-15 16:12:40 +0900

    fixed ssl error of asahi-install

--- asahi-fix/fix-ssl.py
+++ asahi-fix/fix-ssl.py
@@ -1,7 +1,12 @@
 # SPDX-License-Identifier: MIT
+from os import system
+
 print("enter")
 with open("./asahi.sh", "r", encoding="utf-8") as f:
     lines = f.read().replace('tar xf "$PKG"', 'tar xf "$PKG"\ncp ./urlcache-fix.py /tmp/asahi-install/urlcache.py')
 
 with open("./asahi.sh", "w", encoding="utf-8") as f:
     f.write(lines)
+
+print("  Start Installer...")
+system("sh ./asahi.sh")
