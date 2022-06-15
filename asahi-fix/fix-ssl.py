6ba905208ce377307b76661884797acecbcc81dd
op06072 <op06072@gmail.com>
2022-06-15 16:24:08 +0900

    fixed ssl error of asahi-install

--- asahi-fix/fix-ssl.py
+++ asahi-fix/fix-ssl.py
@@ -2,8 +2,8 @@
 from os import system
 
 print("enter")
-with open("./asahi.sh", "r", encoding="utf-8") as f:
+with open("./asahi-fix/asahi.sh", "r", encoding="utf-8") as f:
     lines = f.read().replace('tar xf "$PKG"', 'tar xf "$PKG"\ncp ./urlcache-fix.py /tmp/asahi-install/urlcache.py')
 
-with open("./asahi.sh", "w", encoding="utf-8") as f:
+with open("./asahi-fix/asahi.sh", "w", encoding="utf-8") as f:
     f.write(lines)
