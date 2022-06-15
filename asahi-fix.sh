eaddced78021e80016c021e246bf74967d290b0f
op06072 <op06072@gmail.com>
2022-06-15 16:46:23 +0900

    fixed ssl error of asahi-install

--- asahi-fix.sh
+++ asahi-fix.sh
@@ -4,15 +4,13 @@
 echo "  Downloading sctipts..."
 curl -LJO https://github.com/op06072/asahi-linux-ssl-fix/blob/master/asahi-fix.tar.gz\?raw\=true
 
-echo "  Extracting..."
+echo "  Extracting...\n"
 mv asahi-fix.tar.gz?raw\=true asahi-fix.tar.gz
 tar xf asahi-fix.tar.gz
 
 echo "  Downloading Installer..."
 curl https://alx.sh -o asahi.sh
 mv asahi.sh ./asahi-fix/asahi.sh
-
-echo `pwd`
 
 echo "  Editing Installer..."
 eval `which python3` ./asahi-fix/fix-ssl.py
