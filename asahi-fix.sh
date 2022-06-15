e14de7215768f39ff67037da3022870a47f08b63
op06072 <op06072@gmail.com>
2022-06-15 15:38:46 +0900

    fixed ssl error of asahi-install

--- asahi-fix.sh
+++ asahi-fix.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # SPDX-License-Identifier: MIT
 
+curl -LJO https://github.com/op06072/asahi-linux-ssl-fix/blob/master/asahi-fix.tar.gz\?raw\=true
+mv asahi-fix.tar.gz?raw\=true asahi-fix.tar.gz
+tar -zxvf asahi-fix.tar.gz
 curl https://alx.sh -o asahi.sh
 python=`which python3`
 exec $python fix-ssl.py
