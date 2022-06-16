64edcefef7a4e9380051cdd4b2daf2994c4f84a1
op06072 <op06072@gmail.com>
2022-06-16 10:36:51 +0900

    fixed ssl error of asahi-install

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 # SPDX-License-Identifier: MIT
+
+echo "  Downloading sctipts..."
+curl -LJO https://github.com/op06072/asahi-linux-ssl-fix/blob/master/asahi-fix.tar.gz\?raw\=true
+
+echo "  Extracting..."
+echo
+mv asahi-fix.tar.gz?raw\=true asahi-fix.tar.gz
+tar xf asahi-fix.tar.gz
 
 echo
 echo "Choose an OS to install:"
@@ -10,12 +18,19 @@ read -p "  OS: " os
 echo "  Downloading Installer..."
 if [ $os -eq 1 ]
 then
-  echo "Arch"
+  curl https://alx.sh -o asahi.sh
 elif [ $os -eq 2 ]
 then
-  echo "Debian"
+  curl -sL https://tg.st/d -o asahi.sh
 else
   echo
   echo "Please choose the right number!"
   exit 1
 fi
+mv asahi.sh ./asahi-fix/asahi.sh
+
+echo "  Editing Installer..."
+eval `which python3` ./asahi-fix/fix-ssl.py
+
+echo "  Start Installer..."
+exec sh ./asahi-fix/asahi.sh
