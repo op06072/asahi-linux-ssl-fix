d1e0207659cec01a8b37fe9c3477fc488f9b4c54
op06072 <op06072@gmail.com>
2022-06-16 13:13:32 +0900

    fixed ssl error of asahi-install

--- test.sh
+++ test.sh
@@ -9,29 +9,4 @@ echo
 mv asahi-fix.tar.gz?raw\=true asahi-fix.tar.gz
 tar xf asahi-fix.tar.gz
 
-echo
-echo "Choose an OS to install:"
-echo "  1: Arch Linux (Official)"
-echo "  2: Debian"
-
-eval `which python3` ./asahi-fix/os-picker.py
-os=$(cat ./os.txt)
-
-if [ $os -eq 1 ]
-then
-  curl https://alx.sh -o asahi.sh
-elif [ $os -eq 2 ]
-then
-  curl -sL https://tg.st/d -o asahi.sh
-else
-  echo
-  exit 1
-fi
-rm ./os.txt
-mv asahi.sh ./asahi-fix/asahi.sh
-
-echo "  Editing Installer..."
-eval `which python3` ./asahi-fix/fix-ssl.py
-
-echo "  Start Installer..."
-exec sh ./asahi-fix/asahi.sh
+exec sh ./asahi-fix/fix-runner.sh
